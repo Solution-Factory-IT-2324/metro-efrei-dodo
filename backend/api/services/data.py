341fe0307a9006b1bc4775324368ea6680aa7445
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-06-23 10:54:23 +0200

    change SQL request to include wheelchair_boarding for list_metro_station

--- backend/api/services/data.py
+++ backend/api/services/data.py
@@ -5,7 +5,7 @@ def get_all_metro_stations():
     cursor = db_connection.cursor(dictionary=True)
 
     query = """
-        SELECT DISTINCT s.stop_id, s.stop_name, s.stop_lat, s.stop_lon, s.zone_id, s.location_type, s.parent_station
+        SELECT DISTINCT s.stop_id, s.stop_name, s.stop_lat, s.stop_lon, s.zone_id, s.wheelchair_boarding
         FROM stops s
         JOIN stop_times st ON s.stop_id = st.stop_id
         JOIN trips t ON st.trip_id = t.trip_id
