89a8519b4afe1e8b9011c08eb3cd7ce698f085cc
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-06-26 19:02:18 +0200

    revert on Update response in case of no data found return 404

--- backend/api/utils/response.py
+++ backend/api/utils/response.py
@@ -8,8 +8,4 @@ def json_response(data=None, message='', status=200):
         'data': data
     }
 
-    if not data:
-        response['message'] = 'No data found'
-        response['status'] = 404
-
     return make_response(jsonify(response), status)
