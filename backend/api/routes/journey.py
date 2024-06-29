457b2942d01048fbca028e2659387eabe7553f08
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-06-29 18:02:40 +0200

    Create Emission CO2 calculation

--- backend/api/routes/journey.py
+++ backend/api/routes/journey.py
@@ -30,3 +30,50 @@ def get_journey_from_to():
         return json_response(data={'path': path, 'distance': distance}, message='Success')
     except Exception as e:
         return json_response(message=f"Error calculating journey: {str(e)}", status=500)
+
+
+@bp.route('/emission/<journey_id>', methods=['GET'])
+def get_journey_emission(journey_id):
+    # Get factors from cache
+    cache_file = 'emission_factors.json'
+    emission_data = get_cache(cache_file, max_age_seconds=604800)
+
+    if emission_data is None:
+        emission_data = get_emission_factors()
+        set_cache(cache_file, emission_data)
+
+    journey_data = get_cache(f'{journey_id}.json', max_age_seconds=25660)
+    if journey_data is None:
+        return json_response(message=f'Journey data with ID {journey_id} not available', status=500)
+
+    graph_data = get_cache('graph.json', max_age_seconds=604800)
+    if graph_data is None:
+        return json_response(message='Graph data not available', status=500)
+
+    try:
+        emission_journey_public_transport, emission_journey_car, distance = emission_calculator(journey_data['path'], emission_data, graph_data)
+        return json_response(data={'emission_journey_public_transport': emission_journey_public_transport,
+                                   'emission_journey_car': emission_journey_car,
+                                   'journey_id': journey_id,
+                                   'distance': distance,
+                                   'Unit emission': 'g CO2 eq.',
+                                   'Unit distance': 'km'
+                                   }, message='Success')
+    except Exception as e:
+        return json_response(message=f"Error calculating journey emission: {str(e)}", status=500)
+
+
+@bp.route('/emission', methods=['GET'])
+def get_emission():
+    cache_file = 'emission_factors.json'
+    emission_data = get_cache(cache_file, max_age_seconds=604800)
+
+    if emission_data is None:
+        return json_response(message='Emission factors not available', status=500)
+
+    try:
+        data = get_emission_factors()
+        set_cache(cache_file, data)
+        return json_response(data=data, message='Success')
+    except Exception as e:
+        return json_response(message=f"Error fetching emission factors: {str(e)}", status=500)
